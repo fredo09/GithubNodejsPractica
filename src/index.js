d9b640ae00045a6122f463e66cd3442b713da230
Fredy <desaMts1806@MacBook-Pro-de-macpro-4.local>
2020-04-25 23:26:40 -0500

    Agregando ruta para desarrollo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
     res.status(200).send('Hola mundo!!');
 });
 
+//Agregando otra ruta en rama de desarrollo
+app.get('/desarrollo', (req, res) =>{
+    res.status(200).send('Hola Desarrollo');
+})
+
 app.listen(PORT , () =>{
     console.log(`Servidor levantado en http://localhost:${PORT}`);
 });
