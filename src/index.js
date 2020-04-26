f1000980e7c95c31eb7c4e342eb116561264d7e3
Fredy <desaMts1806@MacBook-Pro-de-macpro-4.local>
2020-04-25 23:53:11 -0500

    Ahora si es el buen comentario

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.status(200).send('Hola mundo!!');
 });
 
-// Agregando comentario no deseado
+//Levantando el servidor 
 app.listen(PORT , () =>{
     console.log(`Servidor levantado en http://localhost:${PORT}`);
 });
